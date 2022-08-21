0f4db5b463bfad9060621176f1f17a1aece7860c
Ryan <sailantht@gmail.com>
2022-08-21 20:36:10 +0430

    Serializers

---
+++ books/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+
+from .models import Author, Book, Review
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ["id", "name", "pseudonym"]
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = [
+            "id",
+            "author",
+            "title",
+            "description",
+            "price",
+            "publisher",
+            "language",
+            "pages",
+            "isbn",
+            "cover_image",
+        ]
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ["id", "book", "user", "description", "date_added"]
